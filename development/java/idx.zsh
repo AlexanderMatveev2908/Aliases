ae676afd7b20151a954e79046d80e141e53835a4
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-07-07 08:25:52 +0300

    main => _

--- development/java/idx.zsh
+++ development/java/idx.zsh
@@ -4,6 +4,14 @@ jr(){
   # java -cp target/classes dev.ninja.App
   
   # compile and run jar
-  mvn package -Dmaven.test.skip=true && \
+  mvn package -q -Dmaven.test.skip=true && \
   java -jar target/app-1.0-SNAPSHOT.jar
+}
+
+jb(){
+  idea
+}
+
+ji(){
+ mvn compile
 }
